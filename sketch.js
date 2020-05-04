1b7b9672b29a7e19d9ebfa7864199909dbd61e23
PranavAmba <64266415+PranavAmba@users.noreply.github.com>
2020-05-04 09:59:28 +0530

    uploaded files

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var car,wall;
+var speed,weight
+
+function setup() {
+  createCanvas(1600,400);
+  
+  speed=random(55,90);
+  weight=random(400,1500);
+
+  car=createSprite(50,200,50,50);
+  wall=createSprite(1500,200,60,400);
+
+  wall.shapeColor=(80,80,80);
+
+  car.velocityX=speed;
+
+  console.log(speed);
+  console.log(weight);
+
+  stopWhenCollided(car,wall);
+}
+
+function draw() {
+  background(255,25,25);  
+  drawSprites();
+}
+function stopWhenCollided(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2) {
+  object1.velocityX = object1.velocityX =0;
+}
+
+}
